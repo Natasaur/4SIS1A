e2a9eaa18caea5b910026d5d29a3e0c34c07dbae
Natasaur <n.garcia.s787@edu.utc.mx>
2023-09-22 05:27:16 -0600

    Avance de ejercicios 1 a 6

--- 02EntradaDatos/ConvertidorTemp.java
+++ 02EntradaDatos/ConvertidorTemp.java
@@ -2,53 +2,95 @@
 //de kilogramos a libras y gramos, y unidades de velocidad de m/s a km/h.
 import java.util.Scanner;
 public class ConvertidorTemp {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int op;
-        float metro, kg, lb, gr, v;
+	public static void main(String[] args) {
+   	Scanner scan = new Scanner(System.in);
+      int op;
+      float metro, kg, v;
 
-        do {
-            op=0;
-            System.out.print("\033[H\033[2J");
-            System.out.println("Seleccione una de las siguientes opciones: ");
-            System.out.println("1. Convertir unidades de metros a centímetros y pulgadas.");
-            System.out.println("2. Convertir kilogramos a libras y gramos.");
-            System.out.println("3. Convertir unidades de velocidad de m/s a km/hr.");
-            System.out.println("4. Salir");
-            op = scan.nextInt();
-            System.out.print("\033[H\033[2J");
-            switch (op) {
-                case 1:
-                do {
-                    System.out.print("Ingrese la cantidad de metros a convertir: ");
-                    metro = scan.nextInt();
-                    longitud(metro);
-                    op=0;
-                    System.out.println("¿Repetir Ejercicio?");
-                    System.out.println("1. Si");
-                    System.out.println("2. No");
-                    op = scan.nextInt();
-                    System.out.print("\033[H\033[2J");
-                } while (op!=2);
-                    break;
+      do {
+         op=0;
+         //System.out.print("\033[H\033[2J");
+         System.out.println("Seleccione una de las siguientes opciones: ");
+         System.out.println("1. Convertir unidades de metros a centímetros y pulgadas.");
+       	System.out.println("2. Convertir kilogramos a libras y gramos.");
+         System.out.println("3. Convertir unidades de velocidad de m/s a km/hr.");
+         System.out.println("4. Salir");
+         op = scan.nextInt();
+         System.out.print("\033[H\033[2J");
+         switch (op) {
+				case 1:
+            do {
+					System.out.print("Ingrese la cantidad de metros a convertir: ");
+               metro = scan.nextInt();
+               longitud(metro);
+               op=0;
+               System.out.println("¿Repetir Ejercicio?");
+               System.out.println("1. Si");
+               System.out.println("2. No");
+               op = scan.nextInt();
+               System.out.print("\033[H\033[2J");
+            } while (op!=2);
+               break;
+            case 2:
+				do{
+					System.out.print("Ingrese la cantidad de kilogramos a convertir: ");
+               kg = scan.nextInt();
+               peso(kg);
+               op=0;
+               System.out.println("¿Repetir Ejercicio?");
+               System.out.println("1. Si");
+               System.out.println("2. No");
+               op = scan.nextInt();
+               System.out.print("\033[H\033[2J");
+				} while (op!=2);
+					break;
+            case 3:
+				do{
+					System.out.print("Ingrese la velocidad en m/s: ");
+               v = scan.nextInt();
+               velocidad(v);
+               op=0;
+               System.out.println("¿Repetir Ejercicio?");
+               System.out.println("1. Si");
+               System.out.println("2. No");
+               op = scan.nextInt();
+               System.out.print("\033[H\033[2J");
+				} while (op!=2);
+					break;
             
-                default:
-                if (op!=4 && op > 4){ //variable usada para salir
-                    System.out.println("Selecciona una de las opciones anteriores.");
-                }
-                    break;
-            }
+            default:
+				if (op!=4 && op > 4){ //variable usada para salir
+					System.out.println("Selecciona una de las opciones anteriores.");
+				}
+					break;
+      	}
             
-        } while (op!=4);
-    }
-    
-    public static void longitud(float metro){
-        float cm;
-        double plg;
-        cm = metro*100;
-        plg = cm / 2.54;
-        System.out.println("Metros: " + metro);
-        System.out.println("Centímetros: " + cm);
-        System.out.println("Pulgadas: " + plg);
-    }
+      } while (op!=4);
+   }
+   
+   public static void longitud(float metro){
+		float cm;
+      double plg;
+      cm = metro*100;
+      plg = cm / 2.54;
+      System.out.println("Metros: " + metro);
+      System.out.println("Centímetros: " + cm);
+      System.out.println("Pulgadas: " + plg);
+   }
+
+   public static void peso(float kg){
+		float gr;
+      double lb;
+      gr = kg*1000;
+      lb =  gr / 453.59;
+      System.out.println("Kilogramos: " + kg);
+      System.out.println("Gramos: " + gr);
+      System.out.println("Libras: " + lb);
+   }
+
+   public static void velocidad(float v){
+      double vl;
+      vl = v*3.6;
+      System.out.println(v + " m/s = " + vl + " km/hr.");
+   }
 }
--- 03EstructurasControl/EstructurasControl.java
+++ 03EstructurasControl/EstructurasControl.java
@@ -1,15 +1,4 @@
-/*Menú de seleccion para 14 opciones
- * 1. Bono o descuento por edad
- * 2. Convertir números de cimales a binarios
- * 3. Convertir temperaturas
- * 4. Número de positivos y negativos
- * 5. Tiendita
- * 6.
- * .
- * .
- * .
- * 14.Salir
- */
+
 import java.util.Scanner;
 
 public class EstructurasControl {
@@ -24,13 +13,13 @@ public class EstructurasControl {
       //Se realiza para no tener valores "basura"
       Scanner entrada = new Scanner(System.in); //Crea una instancia del objeto de la entrada por defecto de la computadora para leer caracteres.
 
-
-
-      int op, fl = 0, numbinario, total, catidadproducto, num1;
+      int op, flag, numbinario, total, cantidadproducto, num1, edad, fahrenheit, n, positivos, negativos, base, altura, area, perimetro;
       float precio, resultado = 0, compra = 0;
+      double celsius, kelvin, rankine;
       String binario = "";
       String nombreproducto;
       char letra;
+      final double PI = 3.1416;
 
       do {
 			op = 0;
@@ -55,6 +44,33 @@ public class EstructurasControl {
          switch (op) {
 				case 1:
 				//Descuento
+            do{
+               System.out.print("Ingrese su edad: ");
+               edad = entrada.nextInt();
+
+               if (edad > 65) {
+                  System.out.println("Se aplica descuento del 40%.");
+               } else if (edad < 21) {
+                  System.out.println("¿Sus padres son socios?");
+                  System.out.println("'Si' = s     'No' = n");
+                  letra = entrada.next().charAt(0);
+                  if (letra == 's' || letra == 'S') {
+                     System.out.println("Se aplica descuento del 45%.");
+                  } else {
+                     System.out.println("Se aplica descuento del 25%.");
+                  }
+               } else {
+                  System.out.println("Se aplica descuento del 25%.");
+               }
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);
 					break;
             case 2:
             //decimal a binario
@@ -76,47 +92,255 @@ public class EstructurasControl {
 						System.out.print("No se puede convertir un número negativo.");
                }
                System.out.println("El número binario es: " + binario);
+
                op=0;
-               System.out.print("\033[H\033[2J");
+               System.out.println("");
                System.out.println("¿Repetir el ejercicio?");
                System.out.println("1. Sí");
                System.out.println("2. No");
-               op = scan.nextInt();
+               op = entrada.nextInt();
                System.out.print("\033[H\033[2J"); //Clear Screen
             }while(op!=2);
 					break;
 				case 3:
+            // Convertidor de temperaturas
+            do{
+               flag = 0;
+               System.out.print("Ingrese los grados Fahrenheit a convertir; ");
+               fahrenheit = entrada.nextInt();
+               System.out.println("Seleccione alguna de las siguientes opciones: ");
+               System.out.println("1. Celsius");
+               System.out.println("2. Kelvin");
+               System.out.println("3. Rankine");
+               flag = entrada.nextInt();
+               switch (flag) {
+                  case 1:
+                  celsius = fahrenheit * -17.77;
+                  System.out.println("Fahrenheit: " + fahrenheit);
+                  System.out.println("Celsius: " + celsius);
+                     break;
+                  case 2:
+                  kelvin = fahrenheit * 255.372;
+                  System.out.println("Fahrenheit: " + fahrenheit);
+                  System.out.println("Kelvin: " + kelvin);
+                     break;
+                  case 3:
+                  rankine = fahrenheit * 491.67;
+                  System.out.println("Fahrenheit: " + fahrenheit);
+                  System.out.println("Rankine: " + rankine);
+                     break;
+               
+                  default:
+                  if ( flag > 3 || flag < 1 ){ //variable usada para salir
+					System.out.println("Selecciona una de las opciones anteriores.");
+            }
+                     break;
+               }
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);
                     
                break;
             case 4:
-                    
+            do{
+               //Positivos negativos
+               positivos = 0;
+               negativos = 0;
+               System.out.print("Ingrese la cantidad de números a ingresar: ");
+               n = entrada.nextInt();
+               System.out.println("Ingrese sus números consecutivamente: ");
+               for (int i = 0; i < n; i++) {
+                  num1 = entrada.nextInt();
+                  if (num1 > 0) {
+                     positivos = positivos + 1;
+                  } else if (num1 < 0) {
+                     negativos = negativos + 1;
+                  }
+               }
+               System.out.println("Positivos: " + positivos);
+               System.out.println("Negativos: " + negativos);
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);                  
                break;
             case 5:
-                    
+            do{
+               //Tiendita
+               System.out.println("Ingrese la cantidad de productos: ");
+               total = entrada.nextInt();
+               for (int i = 0; i <= total; i++) {
+                  System.out.println("Ingrese el nombre del producto: ");
+                  nombreproducto = entrada.next();
+                  System.out.println("Ingrese el precio del producto: ");
+                  precio = entrada.nextFloat();
+                  System.out.println("Ingrese la cantidad del producto: ");
+                  cantidadproducto = entrada.nextInt();
+                  resultado = precio * cantidadproducto;
+                  compra = compra + resultado;
+               }
+               System.out.println("El total de la compra es: " + compra);
+
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);                    
                break;
             case 6:
-                    
+            do{
+               //Area, perimetro y volumen
+               System.out.print(PI);
+               flag = 0;
+               System.out.println("Seleccione alguna de las siguientes opciones: ");
+               System.out.println("1. Área y Perímetro de un rectángulo.");
+               System.out.println("2. Área y Perímetro de un triángulo.");
+               System.out.println("3. Volúmen de una esfera.");
+               System.out.println("4. Volúmen de un cilindro.");
+               flag = entrada.nextInt();
+               switch (flag) {
+                  case 1:
+                  System.out.println("Ingrese el valor de la base: ");
+                  base = entrada.nextInt();
+                  System.out.println("Ingrese el valor de la altura: ");
+                  altura = entrada.nextInt();
+                  area = base * altura;
+                  perimetro = 2 * (base + altura);
+                  System.out.println("Área: " + area);
+                  System.out.println("Perímetro: " + perimetro);
+                     break;
+                  case 2:
+                  System.out.println("Ingrese el valor de la base: ");
+                  base = entrada.nextInt();
+                  System.out.println("Ingrese el valor de la altura: ");
+                  altura = entrada.nextInt();
+                  area = (base * altura)/2;
+                  perimetro = 2 * (base + altura);
+                  System.out.println("Área: " + area);
+                  System.out.println("Perímetro: " + perimetro);
+                     
+                     break;
+                  case 3:
+                     
+                     break;
+                  case 4:
+                     
+                     break;
+               
+                  default:
+                  if ( flag > 3 || flag < 1 ){ //variable usada para salir
+				   	System.out.println("Selecciona una de las opciones anteriores.");
+                  }
+                     break;
+               }
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);  
                break;
          	case 7:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);   
                break;
             case 8:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);  
                break;
             case 9:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);  
                break;
             case 10:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);   
                break;
             case 11:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);  
                break;
             case 12:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);   
                break;
             case 13:
-                    
+            do{
+
+               op=0;
+               System.out.println("");
+               System.out.println("¿Repetir el ejercicio?");
+               System.out.println("1. Sí");
+               System.out.println("2. No");
+               op = entrada.nextInt();
+               System.out.print("\033[H\033[2J"); //Clear Screen
+            }while(op!=2);  
                break; 
             
             default:
@@ -127,8 +351,7 @@ public class EstructurasControl {
          	}//Switch
 
 
-            //letra = entrada.next().charAt(0);
-        } while (letra != 14);
+        } while (op != 14); //letra = entrada.next().charAt(0);
 
 
     }
