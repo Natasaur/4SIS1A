38ade09a683267d3db172cc75901253ea82507b1
Natasaur <n.garcia.s787@edu.utc.mx>
2023-10-24 10:38:29 -0600

    Tarea Cálculo de Áreas y Perímetros 24/10/23

--- FigurasHerencia/Menu.java
+++ FigurasHerencia/Menu.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class Menu {
     public void mostrarMenu() {
         int op;
-        float base, altura, radio, lado;
+        float base = 0, altura = 0, radio = 0, lado = 0;
+        boolean valid = false;
+        String strInput1, strInput2, strInput3;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Seleccione una de las opciones: \n"
                             + "1. Rectángulo \n"
@@ -12,36 +14,83 @@ public class Menu {
         op = entrada.nextInt();
         switch (op) {
             case 1://Rectángulo
-            System.out.print("Ingrese el valor de la base: ");
-            base = entrada.nextFloat();
-            System.out.print("Ingrese el valor de la altura: ");
-            altura = entrada.nextFloat();
+            while (valid == false) {
+                System.out.print("Ingrese el valor de la base: ");
+                strInput1 = entrada.next();
+                System.out.print("Ingrese el valor de la altura: ");
+                strInput2 = entrada.next();
+                try {
+                    base = Float.parseFloat(strInput1);
+                    altura = Float.parseFloat(strInput2);
+                    valid = true;
+                } catch (Exception e) {
+                    System.out.println("Ingresa solo números");
+                    System.out.println("Error: " + e.getMessage());
+                    System.out.println();
+                }
+            }
             Rectangulo rectangulo = new Rectangulo(base, altura);
             rectangulo.mostrarRectangulo();
+            valid = false;
                 break;
             case 2://Círculo
-            System.out.print("Ingrese el valor del radio: ");
-            radio = entrada.nextFloat();
+            while (valid == false) {
+                System.out.print("Ingrese el valor del radio: ");
+                strInput1 = entrada.next();
+                try {
+                    radio = Float.parseFloat(strInput1);
+                    valid = true;
+                } catch (Exception e) {
+                    System.out.println("Ingresa solo números");
+                    System.out.println("Error: " + e.getMessage());
+                    System.out.println();
+                }
+            }
             Circulo circulo = new Circulo(radio);
             circulo.mostrarCirculo();
+            valid = false;
                 break;
             case 3://Triángulo
-            System.out.print("Ingrese el valor de la base: ");
-            base = entrada.nextFloat();
-            System.out.print("Ingrese el valor de la altura: ");
-            altura = entrada.nextFloat();
+            while (valid = false) {
+                System.out.print("Ingrese el valor de la base: ");
+                strInput1 = entrada.next();
+                System.out.print("Ingrese el valor de la altura: ");
+                strInput2 = entrada.next();
+                try {
+                    base = Float.parseFloat(strInput1);
+                    altura = Float.parseFloat(strInput2);
+                    valid = true;
+                } catch (Exception e) {
+                    System.out.println("Ingresa solo números");
+                    System.out.println("Error: " + e.getMessage());
+                    System.out.println();
+                }
+            }
             Triangulo triangulo = new Triangulo(base, altura);
             triangulo.mostrarTriangulo();
+            valid = false;
                 break;
             case 4://Rombo
-            System.out.print("Ingrese el valor del lado: ");
-            lado = entrada.nextFloat();
-            System.out.print("Ingrese el valor del eje mayor: ");
-            altura = entrada.nextFloat();
-            System.out.print("Ingrese el valor del eje menor: ");
-            base = entrada.nextFloat();
+            while (valid = false) {
+                System.out.print("Ingrese el valor del lado: ");
+                strInput1 = entrada.next();
+                System.out.print("Ingrese el valor del eje mayor: ");
+                strInput2 = entrada.next();
+                System.out.print("Ingrese el valor del eje menor: ");
+                strInput3 = entrada.next();
+                try {
+                    lado = Float.parseFloat(strInput1);
+                    base = Float.parseFloat(strInput2);
+                    altura = Float.parseFloat(strInput3);
+                } catch (Exception e) {
+                    System.out.println("Ingresa solo números");
+                    System.out.println("Error: " + e.getMessage());
+                    System.out.println();
+                }
+            }
             Rombo rombo = new Rombo(base, altura, lado);
             rombo.mostrarRombo();
+            valid = false;
                 break;
         
             default:
--- FigurasHerencia/Principal.java
+++ FigurasHerencia/Principal.java
@@ -1,3 +1,9 @@
+/*
+ * Deberán de realizar un programa utilizando herencia para el cálculo de figuras geométricas.
+ * El programa debe de contar con rectángulo, circulo, triangulo, rombo.
+ * Deberán de calcular el perímetro y área.
+ * Deben de integrar herencia, encapsulamiento y manejo de excepciones.
+ */
 public class Principal {
     public static void main(String[] args) {
         Menu menu = new Menu();
