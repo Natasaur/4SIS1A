a317a9b2b56f271f7927b8c9baf17a9c94f73c93
Natasaur <n.garcia.s787@edu.utc.mx>
2023-10-29 20:47:05 -0600

    Primer programa en NetBeans

---
+++ 10CRUD/11Ventanita/src/pkg11ventanita/LoginButtonListener.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg11ventanita;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author jakeg
+ */
+public class LoginButtonListener implements ActionListener{
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null,"Login button has been pressed");
+    }
+    
+}
