8a05bc3726a8d7a9bc8722c9ea5d5206bd5062fd
Natasaur <n.garcia.s787@edu.utc.mx>
2023-10-30 22:10:49 -0600

    Login con contraseña no visible

---
+++ LoginInterface/Login/src/login/Login.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jakeg
+ */
+public class Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new loginFrame().setVisible(true);
+        //loginFrame.setLocationRelativeTo(null);
+        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
